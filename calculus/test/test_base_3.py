28e562ef8f0f4218df67ce6a3837b9038f901942
bbradacs <bbradacs@gmail.com>
2015-04-21 13:26:23 -0400

    fixed some typos, first test case works great

--- calculus/test/test_base_3.py
+++ calculus/test/test_base_3.py
@@ -16,7 +16,7 @@ class CalculationTestCase(unittest.TestCase):
         result = op(a,b)
         self.assertEqual(result,expected)
 
-    def _test_error(selfself,operation):
+    def _test_error(self,operation):
         self.assertRaises(TypeError,operation,"foo",2)
         self.assertRaises(TypeError,operation,"bar","egg")
         self.assertRaises(TypeError,operation,[3],[8,2])
@@ -24,17 +24,17 @@ class CalculationTestCase(unittest.TestCase):
 
     def test_add(self):
         log.msg('create a calculator')
-        result = calc.add(3, 8)
-        self._test(self.calc.add(3,8,11))
+        result = self.calc.add(3, 8)
+        self._test(self.calc.add,3,8,11)
 
     def test_subtract(self):
-        self._test(self.calc.subtract(7,3,4))
+        self._test(self.calc.subtract,7,3,4)
 
     def test_multiply(self):
-        self._test(self.calc.multiply(6,9,54))
+        self._test(self.calc.multiply,6,9,54)
 
     def test_divide(self):
-        self._test(self.calc.divide(12,5,2))
+        self._test(self.calc.divide,12,5,2)
 
     def test_errorAdd(self):
         self._test_error(self.calc.add)
@@ -43,7 +43,7 @@ class CalculationTestCase(unittest.TestCase):
         self._test_error(self.calc.subtract)
 
     def test_errorMultiply(self):
-        self._test_error(self.calc_multiply)
+        self._test_error(self.calc.multiply)
 
     def test_errorDivide(self):
         self._test_error(self.calc.divide)
