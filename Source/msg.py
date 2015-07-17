5454bda314399024bb3b1b27dead11f710ef47e3
bbradacs <bbradacs@gmail.com>
2015-07-17 11:23:24 -0400

    playin' around

---
+++ Source/msg.py
@@ -0,0 +1,23 @@
+class Op:
+    pass
+    class Receive:
+        pass
+        class REQUEST:
+            pass
+        class ADDRESS_CHECK:
+            pass
+        class ADDRESS_PREPARE:
+            pass
+        class FINISHED_REQUEST:
+            pass
+        class FUNDS_RECEIVED:
+            pass
+        class FUNDS_NOT_RECEIVED:
+            pass
+
+print str(Op.Receive.REQUEST)
+
+
+
+
+
